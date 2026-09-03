a9f57fbd138435d4d217b4818d0501745ff545f3
Ederson <edersonrodrigo31@gmail.com>
2026-09-03 14:28:21 -0300

    feat: search wallet service

--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -1,19 +1,37 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { OpenWalletService } from './services/open-wallet.service';
 import {
   type OpenWalletPayload,
   openWalletSchema,
 } from './schema/open-wallet.schema';
 import { ZodValidationPipe } from '../../common/pipes/zod-validation.pipe';
+import { SearchWalletService } from './services/search-wallet.service';
 
 @Controller('wallets')
 export class WalletController {
-  constructor(private readonly openWalletService: OpenWalletService) {}
+  constructor(
+    private readonly openWalletService: OpenWalletService,
+    private readonly searchWalletService: SearchWalletService,
+  ) {}
 
   @Post()
   async openWallet(
     @Body(new ZodValidationPipe(openWalletSchema)) body: OpenWalletPayload,
   ) {
     return await this.openWalletService.execute(body);
+  }
+
+  @Get(':walletId')
+  async getWallet(@Param('walletId') walletId: string) {
+    return await this.searchWalletService.getWallet(walletId);
+  }
+
+  @Get(':walletId/ledger')
+  async getLedger(
+    @Param('walletId') walletId: string,
+    @Query('cursor') cursor?: string,
+    @Query('limit') limit = 50,
+  ) {
+    return await this.searchWalletService.getLedger(walletId, cursor, limit);
   }
 }
--- src/modules/wallet/wallet.module.ts
+++ src/modules/wallet/wallet.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WalletController } from './wallet.controller';
 import { OpenWalletService } from './services/open-wallet.service';
+import { SearchWalletService } from './services/search-wallet.service';
 
 @Module({
   imports: [],
-  providers: [OpenWalletService],
+  providers: [OpenWalletService, SearchWalletService],
   controllers: [WalletController],
 })
 export class WalletModule {}
